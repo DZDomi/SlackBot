997c86c08f43cb77d312bba6e7b0db8217525770
DZDomi <dzdomi@gmail.com>
2017-07-26 21:51:06 +0200

    forwarder now works correctly with slack response

--- forwarder/forwarder.js
+++ forwarder/forwarder.js
@@ -4,11 +4,11 @@ let server = require('http').createServer(app);
 let io = require('socket.io')(server);
 let config = require('../config.json');
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.post('/slack', function(req, res){
     console.log("Got request from slack:\n" + req.body);
-    io.emit("slack", JSON.stringify(req.body));
+    io.emit("slack", req.body);
     //Respond 200 Everything ok
     res.send();
 });
