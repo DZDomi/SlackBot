296a4b4c67dffba5222d2cc066521edec1a0081f
DZDomi <dzdomi@gmail.com>
2017-07-30 20:04:21 +0200

    test

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function writeGifMessage(message){
             action: Request.getEnum("Action").GIF,
             sender: "I bim 1 sender",
             gifRequest: {
-                gif: data
+                gif: data.toBuffer()
             }
         };
         sendObjectToSocket(object);
