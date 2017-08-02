9ec312268456608842f119b4d23d86980c13e739
DZDomi <dzdomi@gmail.com>
2017-08-03 00:23:31 +0200

    added time check for gifs

--- util/gif.js
+++ util/gif.js
@@ -8,18 +8,20 @@ let gif = {};
 
 gif.downLoadGif = (text, callback) => {
     logger.log("gif", "Searching for gif from phrase: " + text);
+    let start = now();
     giphy.search(text, (err, res) => {
+        let time = (now() - start).toFixed(0);
         if(res.data.length === 0){
-            logger.log("gif", "Unable to find gif from phrase: " + text);
+            logger.log("gif", "Unable to find gif from phrase: " + text + ", took " + time + "ms");
             return callback(new Error("No gif found"));
         }
         shuffle(res.data);
         let selectedGif = res.data[0];
-        logger.log("gif", "Starting to download gif with id: " + selectedGif.id);
-        let start = now();
+        logger.log("gif", "Found gif with id: " + selectedGif.id + " starting download, took " + time + "ms");
+        start = now();
         download(selectedGif.images.original.url)
             .then((data) => {
-                let time = (now() - start).toFixed(0);
+                time = (now() - start).toFixed(0);
                 logger.log("gif", "Finished Downloading gif with id: " + selectedGif.id + ", took " + time + "ms");
                 callback(null, data)
             });
