ae31d110bb1d8832e511a9eeb0bf086d43f60ec0
DZDomi <dzdomi@gmail.com>
2017-08-02 23:56:30 +0200

    rmoeved duplicate function calls

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function tryToReconnect(){
     if(socket.writable){
         return;
     }
+    socket.removeListener("connection");
     logger.log("index", "Socket " + config.serverSocket + " disconnected. Trying to reconnect...");
     socket.connect(config.serverSocket, socketConnected);
 }
