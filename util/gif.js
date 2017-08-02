53f5f47b54712b6d486b5146964c4e86b806d4ff
DZDomi <dzdomi@gmail.com>
2017-08-03 00:40:31 +0200

    added byte size

--- util/gif.js
+++ util/gif.js
@@ -3,6 +3,7 @@ let download = require('download');
 let shuffle = require('shuffle-array');
 let logger = require('./logger');
 let now = require('performance-now');
+let pretty = require('prettysize');
 
 let gif = {};
 
@@ -17,12 +18,13 @@ gif.downLoadGif = (text, callback) => {
         }
         shuffle(res.data);
         let selectedGif = res.data[0];
-        logger.log("gif", "Found gif with id: " + selectedGif.id + " starting download, took " + time + "ms");
+        logger.log("gif", "Found gif with id: " + selectedGif.id + ", took " + time + "ms");
+        logger.log("gif", "Starting download of id: " + selectedGif.id);
         start = now();
         download(selectedGif.images.original.url)
             .then((data) => {
                 time = (now() - start).toFixed(0);
-                logger.log("gif", "Finished Downloading gif with id: " + selectedGif.id + ", took " + time + "ms");
+                logger.log("gif", "Finished Downloading gif with id: " + selectedGif.id + ", took " + time + "ms (" + pretty(data.length) + ")");
                 callback(null, data)
             });
     });
