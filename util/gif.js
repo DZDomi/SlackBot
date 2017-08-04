46de054da31b0dbfa05c6a9b012b4c57f6a1e7c5
DZDomi <dzdomi@gmail.com>
2017-08-04 20:20:21 +0200

    added size check for gifs

--- util/gif.js
+++ util/gif.js
@@ -33,7 +33,6 @@ gif.downLoadGif = (text, callback) => {
 
 function removeLargeGifs(data, size) {
     return data.filter((gif) => {
-        console.log(gif.images.original);
         return gif.images.original.size < size;
     });
 }
